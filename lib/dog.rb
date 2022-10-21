c01be98ca54a2f894098c5f1f5327a0e7517e050
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-10-21 17:50:45 +0300

    Done with assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,15 @@
+#An instance variable is a variable that is accessible in any instance method in a particular instance of a class.
 class Dog
+  def name=(dog_name)
+    @this_dogs_name = dog_name
+  end
 
+  def name
+    @this_dogs_name
+  end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
